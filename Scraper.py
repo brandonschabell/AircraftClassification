28e44cf9a427b29326f9207189afcb3448de7cd3
Brandon Schabell <brandonschabell@gmail.com>
2018-02-04 21:44:51 -0600

    added js for pulling airliners data

--- Scraper.py
+++ Scraper.py
@@ -1,2 +1,8 @@
 # WebScraper for pulling images from www.airliners.net
+
 # AirlinersIDS.csv contains a lookup table for IDs:Aircraft Name
+
+# CSV Data was pulled using the following javascript on www.airliners.net/search:
+#   $x('//*[@class="selectize-dropdown-content"]//div').forEach(function(item) {
+#       console.log(item.getAttribute("data-value") + "," + item.innerText)
+#   })
