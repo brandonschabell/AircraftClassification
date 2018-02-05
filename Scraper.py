5422c86fb508bf22d0c55cd6e6f2d3a9ef35b9db
Brandon Schabell <brandonschabell@gmail.com>
2018-02-04 21:58:54 -0600

    added scraper js

--- Scraper.py
+++ Scraper.py
@@ -6,3 +6,10 @@
 #   $x('//*[@class="selectize-dropdown-content"]//div').forEach(function(item) {
 #       console.log(item.getAttribute("data-value") + "," + item.innerText)
 #   })
+
+# Search is of form:
+#   http://www.airliners.net/search?aircraftBasicType={ID}&sortBy=dateAccepted&sortOrder=desc&perPage=84&display=card
+# Where {ID} corresponds to the ID found in AirlinersIDS.csv
+
+# On each page, images can be found with:
+#   $x("//*[@class='card-image']//a//img").forEach(function(item) {console.log(item.src)})
