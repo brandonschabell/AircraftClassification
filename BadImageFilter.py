cb5c0a1c3440b05aa6b27d2ed1f1a3d90b649362
Brandon Schabell <brandonschabell@gmail.com>
2018-02-09 09:37:05 -0600

    updated model

---
+++ BadImageFilter.py
@@ -0,0 +1,28 @@
+import os
+
+train_directory = os.getcwd() + os.sep + "Images"
+test_directory = os.getcwd() + os.sep + "TestImages"
+plane_list_train = os.listdir(train_directory)
+plane_list_test = os.listdir(test_directory)
+
+for plane in plane_list_train:
+    file_path = train_directory + os.sep + plane
+    file_list = os.listdir(file_path)
+    for f in file_list:
+        f_path = file_path + os.sep + f
+        size = os.path.getsize(f_path)
+        if size < 300:
+            # Corrupt file, delete this
+            os.remove(f_path)
+            print(f_path)
+
+for plane in plane_list_test:
+    file_path = test_directory + os.sep + plane
+    file_list = os.listdir(file_path)
+    for f in file_list:
+        f_path = file_path + os.sep + f
+        size = os.path.getsize(f_path)
+        if size < 300:
+            # Corrupt file, delete this
+            os.remove(f_path)
+            print(f_path)
